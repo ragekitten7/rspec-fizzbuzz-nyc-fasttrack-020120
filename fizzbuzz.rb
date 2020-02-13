7bfa6e9180446b8787a6460dfd7604ea145ab58d
Sam Bryant <bryantsj@gmail.com>
2020-02-13 00:58:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 require_relative './spec_helper.rb'
-
+whatever = 3
 def fizzbuzz(whatever)
   if whatever % 3 == 0
     puts "Fizz"
